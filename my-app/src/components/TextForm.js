8e610e535f86a4881c04daaa295edde3660d47ca
MarcusAurelius29 <varungole1999@gmail.com>
2023-04-10 14:58:19 +0530

    The logic of word counting, chars , time taken to read

---
+++ my-app/src/components/TextForm.js
@@ -0,0 +1,47 @@
+import React , {useState} from 'react'
+
+export default function TextForm(props) {
+   const [text , setText] = useState(" ")
+   
+   const handleUpClick = () => {
+     let newText = text.toUpperCase();
+     setText(newText);
+   }
+
+   const handleOnChange = (event) => {
+      setText(event.target.value);
+    
+    }
+
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+   
+
+    return (
+        <>
+    <div className="container">
+      <h2>{props.heading}</h2>
+      <div className="mb-3">
+            <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+      </div>
+
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+
+      <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+
+    </div>
+
+    <div className="container">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p> {0.008 * text.split(" ").length } Minutes to read this</p>
+
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+
+    </>
+  )
+}
